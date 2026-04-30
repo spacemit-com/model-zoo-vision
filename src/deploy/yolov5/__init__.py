ced1a27e52283b51eae4fafafb5967cda94b5361
sugarsf <561496700@qq.com>
2026-04-30 17:56:11 +0800

    feat(vision): add_yolov5_python_code_and_add_camera_parameter
    Signed-off-by: sugarsf <561496700@qq.com>

---
+++ src/deploy/yolov5/__init__.py
@@ -0,0 +1,11 @@
+# Copyright 2026 SpacemiT (Hangzhou) Technology Co. Ltd.
+#
+# SPDX-License-Identifier: Apache-2.0
+
+"""
+YOLOv5 Object Detection Model
+"""
+
+from .python import YOLOv5Detector
+
+__all__ = ['YOLOv5Detector']
---
+++ src/deploy/yolov5/python/__init__.py
@@ -0,0 +1,7 @@
+# Copyright 2026 SpacemiT (Hangzhou) Technology Co. Ltd.
+#
+# SPDX-License-Identifier: Apache-2.0
+
+from .yolov5_detector import YOLOv5Detector
+
+__all__ = ['YOLOv5Detector']
